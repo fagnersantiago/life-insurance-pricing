5c157c63f2396ad5b7319548bcf4c7f7242d89a7
Fagner Santiago <fagner.santiago@hotmail.com>
2023-11-07 00:47:30 -0300

    feat reorganizing folders project

--- prisma/migrations/20231103121127_inserting_limit_in_password_user/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `password` on the `user` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(60)`.
-
-*/
--- AlterTable
-ALTER TABLE "user" ALTER COLUMN "password" SET DATA TYPE VARCHAR(60);
--- src/users/dto/create-user.dto.ts
+++
@@ -1,9 +0,0 @@
-import { Rule } from '../entitie/user';
-
-export class CreateUserDto {
-  userId?: string;
-  userName: string;
-  password: string;
-  rule?: Rule;
-  createdAt?: Date;
-}
